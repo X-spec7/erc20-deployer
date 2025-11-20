2dc3fa88c28348fca12b7874ae5e991e4c223517
X-spec7 <mercury.spec77@gmail.com>
2025-11-20 12:09:59 +0900

    feat: setup hardhat config for sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -28,11 +28,17 @@ export default defineConfig({
       type: "edr-simulated",
       chainType: "op",
     },
+    // sepolia: {
+    //   type: "http",
+    //   chainType: "l1",
+    //   url: configVariable("SEPOLIA_RPC_URL"),
+    //   accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    // },
     sepolia: {
       type: "http",
       chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+      url: "https://sepolia.infura.io/v3/39ed309616e14e7195e8b06751e40938",
+      accounts: ["1b121b5f7c2d8e4d4cecd831fc7e05ecbcd3ea4117919a02ddc32d296324fb23"],
     },
   },
 });
